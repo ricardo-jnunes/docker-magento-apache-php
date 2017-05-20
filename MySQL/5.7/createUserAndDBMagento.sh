d8fbc59abe01d3b0293b1a0ac622b5ed00903092
Ricardo J. Nunes <jnunes.ricardo@gmail.com>
2017-05-20 15:10:19 -0300

    From official images.

--- MySQL/5.7/createUserAndDBMagento.sh
+++ MySQL/5.7/createUserAndDBMagento.sh
@@ -9,7 +9,9 @@ if [ -f /root/.my.cnf ]; then
 
     mysql -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
     mysql -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    mysql -e "CREATE USER ${MAINDB}@'%' IDENTIFIED BY '${PASSWDDB}';"
     mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'%';"
     mysql -e "FLUSH PRIVILEGES;"
 
 # If /root/.my.cnf doesn't exist then it'll ask for root password   
